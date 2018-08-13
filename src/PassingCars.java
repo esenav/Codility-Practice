eb8417fccb22d9c7df0761fede16ce2634c71e3c
esenav <evaldas.senavaitis@gmail.com>
2018-08-13 18:25:46 +0100

    Renamed carWest to carEast as first it was naming mistake as we counted cars traveling east meaning "0s".

--- src/PassingCars.java
+++ src/PassingCars.java
@@ -7,11 +7,11 @@ public class PassingCars {
     // https://app.codility.com/demo/results/trainingGTFWGZ-FHX/
     private static int solution(int[] A){
         int MAX = 1000000000;
-        int carWest = 0;
+        int carEast = 0;
         int carPassing = 0;
         for (int aA : A) {
-            carWest = carWest + 1 - aA;
-            carPassing += carWest * aA;
+            carEast = carEast + 1 - aA;
+            carPassing += carEast * aA;
         }
         if(carPassing > MAX || carPassing < 0 ){
             return -1;
